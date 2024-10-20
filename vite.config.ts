0b74e5614218d6b4fb8de3878fd47749cf845bfc
Peter Stovka <stovka.peter@gmail.com>
2024-10-20 13:50:41 +0200

    wip unplugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
-import components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
   plugins: [
@@ -17,23 +16,5 @@ export default defineConfig({
         }
       }
     }),
-    // components({
-    //   globs: [
-    //     'resources/js/Components/**/*.vue',
-    //     'resources/js/Layouts/**/*.vue',
-    //
-    //     '!resources/js/Components/DataTable/Columns/*.vue',
-    //     '!resources/js/Components/DataTable/Filters/*.vue',
-    //     '!resources/js/Components/DataTable/ActionList.vue',
-    //     '!resources/js/Components/DataTable/EmptyPattern.vue',
-    //   ],
-    //   resolvers: [
-    //     componentName => {
-    //       if (['Link', 'Head'].includes(componentName)) {
-    //         return { name: componentName, from: '@inertiajs/vue3' }
-    //       }
-    //     }
-    //   ]
-    // })
   ]
 })
