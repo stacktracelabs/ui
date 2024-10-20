a83fe2f35001ecade48c665db4ebaa5e6a0045b5
Peter Stovka <stovka.peter@gmail.com>
2024-10-20 15:57:35 +0200

    wip

--- docs/.vitepress/theme/components/ComponentPreview.vue
+++ docs/.vitepress/theme/components/ComponentPreview.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
-    <component v-if="component" :is="component" />
-
-    <code v-if="code">{{ code }}</code>
-  </div>
+  <Tabs default-value="preview">
+    <TabsList>
+      <TabsTrigger value="preview">Preview</TabsTrigger>
+      <TabsTrigger value="code">Code</TabsTrigger>
+    </TabsList>
+    <TabsContent value="preview">
+      <div class="bg-background py-16 px-8 border rounded">
+        <component v-if="component" :is="component" />
+      </div>
+    </TabsContent>
+    <TabsContent value="code">
+      <div class="bg-background border rounded overflow-scroll p-4">
+        <div v-if="codeHtml" v-html="codeHtml"></div>
+      </div>
+    </TabsContent>
+  </Tabs>
 </template>
 
 <script setup lang="ts">
 import { onMounted, shallowRef, type Component, ref } from "vue";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/Components/Tabs'
+import { codeToHtml, createCssVariablesTheme } from 'shiki'
 
 const props = defineProps<{
   name: string
@@ -15,10 +28,18 @@ const props = defineProps<{
 
 const component = shallowRef<Component>()
 const code = ref<string>()
+const codeHtml = ref<string>()
 
 onMounted(async () => {
   component.value = await import((`../../previews/${props.name}.vue`)).then(it => it.default)
 
   code.value = await import((`../../previews/${props.name}.vue?raw`)).then(it => it.default.trim())
+  codeHtml.value = await codeToHtml(code.value, {
+    lang: 'vue',
+    theme: createCssVariablesTheme({
+      variablePrefix: '--shiki-',
+      variableDefaults: {},
+    })
+  })
 })
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ export default {
     "./storage/framework/views/*.php",
     "./resources/views/**/*.blade.php",
     "./resources/js/**/*.{ts,tsx,vue}",
+    "./docs/.vitepress/**/*.{ts,tsx,vue}",
   ],
 
   theme: {
